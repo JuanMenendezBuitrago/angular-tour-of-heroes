4fd2e66109a0cf6ace77c9980aefc7a30d5f4c3f
Juan Menendez <juan.menendez.buitrago@gmail.com>
2017-01-13 10:03:57 +0100

    prepare view to edit our hero

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
+import { Hero } from './shared/models/hero';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  	<h1>{{ title }}</h1>
+  	<h2>{{ hero.name }} details!</h2>
+  	<div>
+  		<label>id: </label> {{ hero.id }}
+  	</div>
+  	<div>
+		<label>name: </label> <input type="text" value="{{ hero.name }}" placeholder="name" />
+  	</div>`,
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  { 
+	title = 'Tour of heroes'; 
+	hero: Hero = {
+		id: 1,
+		name: 'Windstorm'
+	}
+}
