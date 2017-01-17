3e275fc49aaaf807badf518ef2d9e942f2e2cb6a
Juan Menendez <juan.menendez.buitrago@gmail.com>
2017-01-17 08:31:02 +0100

    add routing

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,19 +1,35 @@
-import { Component, Input } from '@angular/core';
+// Keep the Input import for now, we'll remove it later:
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
+
+import { HeroService } from './hero.service';
 import { Hero } from './shared/models/hero';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
-	selector: 'my-hero-detail',
-	template: `
-	<div *ngIf="hero">
-		<h2>{{hero.name}} details!</h2>
-	    <div><label>id: </label>{{hero.id}}</div>
-	    <div>
-	        <label>name: </label>
-	        <input [(ngModel)]="hero.name" placeholder="name"/>
-		</div>
-    </div>`
+  moduleId: module.id,
+  selector: 'my-hero-detail',
+  templateUrl: 'hero-detail.component.html',
+  styleUrls: ['hero-detail.component.css']
 })
-export class HeroDetailComponent {
-	@Input()
-	hero: Hero;
+export class HeroDetailComponent implements OnInit {
+	@Input() hero: Hero;
+
+	constructor(
+		private heroService: HeroService,
+		private route: ActivatedRoute,
+		private location: Location
+	) {}
+
+	ngOnInit(): void {
+		console.log(this.route.params);
+		this.route.params
+	    .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+	    .subscribe(hero => this.hero = hero);
+	}
+
+	goBack(): void {
+	  this.location.back();
+	}
 }
