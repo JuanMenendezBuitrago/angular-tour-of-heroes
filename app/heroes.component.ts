1b6946608bb7e3ebd06f1a12545d57a6f9c5c8d1
Juan Menendez <juan.menendez.buitrago@gmail.com>
2017-01-17 08:31:02 +0100

    final commit

---
+++ app/heroes.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Hero } from './shared/models/hero';
+import { HeroService} from './hero.service';
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-heroes',
+  templateUrl: 'heroes.component.html',
+  styleUrls: ['heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit { 
+	heroes: Hero[];
+	selectedHero: Hero;
+
+	constructor(
+		private heroService: HeroService, 
+		private router: Router) {}
+
+	ngOnInit(): void {
+		this.getHeroes();
+	}
+
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
+		console.log(hero);
+	}
+
+	getHeroes(): void {
+    	// this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    	this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  	}
+
+  	gotoDetail(): void {
+	  this.router.navigate(['/detail', this.selectedHero.id]);
+	}
+}
